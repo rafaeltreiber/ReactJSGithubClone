2f9debb5ad258b737f8da385ab67ac2b3ef3625b
Rafael Treiber <rafaeltreiber@gmail.com>
2021-01-07 16:17:03 -0300

    iniciando fetch

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, ForkIcon, LinkButton, GithubIcon } from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to = {'/rafaeltreiber'}>rafaeltreiber
+        </Link>
+        <span>/</span>
+        <Link className = {'reponame'} to = {'/rafaeltreiber/segredo'}>segredo</Link>
+      </Breadcrumb>
+      <p>Um joguinho para aprender Ionic + Firebase</p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+        <ForkIcon />
+        <b>0</b>
+        <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href = {'https://github.com/rafaeltreiber/segredo'}>
+        <GithubIcon />
+        <span>Ver no Github</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
